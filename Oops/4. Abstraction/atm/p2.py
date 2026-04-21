e713bb7ca3991dd0f8d3c2d97db9ce556762bcd9
suryareddy127 <suryanarayanareddypatil@gmail.com>
2026-04-21 22:17:23 +0530

    updated p2 module

--- Oops/4. Abstraction/atm/p2.py
+++ Oops/4. Abstraction/atm/p2.py
@@ -1,40 +1,48 @@
 from p1 import ATM
 
 class SBI_ATM(ATM):
+    balance = 2000
     def insert_card(self,i):
         self.i =i
         if i == "insertCard":
             print("Card Inserted")
         else:
             print("First insert the card")
-        # print(f"successfull {self.i}")
+        
 
     def enter_Pin(self, pin):
         self.pin = pin
-        if pin!=0:
+        if pin!=0 and pin>=1000 and pin<=9999 :
             print("pin verified")
         else:
             print("enter the correct pin")
     
     def withdraw_cash(self, amount):
-        return super().withdraw_cash(amount)
-    
+        self.amount =amount
+        if amount>=500 and amount<=self.balance:
+            print("proceed for withdraw")
+            print(f"{amount}  withdraw Success....")
+        else:
+            print(f"Enter the amount of minimum 500 or below the {self.balance} balance")
+
     def deposit_amount(self, amount):
-        return super().deposit_amount(amount)
+        self.amount= amount
+        print(f"{amount}  deposited Success....")
     
-    def check_balance(self):
-        return super().check_balance()
+    def check_balance(self):    
+        print(f"Balance viewed: {self.balance-self.amount}")
     
     def transaction_history(self):
-        return super().transaction_history()
+        print("Transactions history")
     
     def payment_success(self):
-        return super().payment_success()
+        print("Successfully Completed")
     
 
 
 mytrans = SBI_ATM()
 mytrans.insert_card("insertCard")
-mytrans.enter_Pin(1231)
-mytrans.withdraw_cash(2000)
-mytrans.payment_success
+mytrans.enter_Pin(int(input("pin: ")))
+mytrans.withdraw_cash(int(input("withdrwal amount: ")))
+mytrans.check_balance()
+mytrans.payment_success()
